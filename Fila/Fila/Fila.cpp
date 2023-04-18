b6f7c0769baf6ad12b7b1cccb64d3886dcd6c72d
sergio <sergiogentilesi@gmail.com>
2023-04-18 12:51:34 -0300

    termino

--- Fila/Fila/Fila.cpp
+++ Fila/Fila/Fila.cpp
@@ -15,6 +15,7 @@ void menu();
 void inicializar();
 void insere();
 void remove();
+void exibirElementos();
 //--------------------------
 
 
@@ -26,14 +27,15 @@ int main()
 void menu()
 {
 	int op = 0;
-	while (op != 4) {
+	while (op != 5) {
 		system("cls"); // somente no windows
 		cout << "Menu Fila";
 		cout << endl << endl;
 		cout << "1 - Inicializar Fila \n";
 		cout << "2 - Inserir elemento \n";
 		cout << "3 - Remover elemento  \n";
-		cout << "4 - Sair \n";
+		cout << "4 - Exibir elemento \n";
+		cout << "5 - Sair \n";
 
 		cout << "Opcao: ";
 		cin >> op;
@@ -42,11 +44,13 @@ void menu()
 		{
 		case 1: inicializar();
 			break;
-		case 2:insere();
+		case 2: insere();
 			break;
 		case 3: remove();
 			break;
-		case 4:
+		case 4: exibirElementos();
+			break;
+		case 5:
 			return;
 		default:
 			break;
@@ -88,13 +92,53 @@ void insere()
 	cin >> novo->valor;
 	novo->prox = NULL;
 
+	if (inicio == NULL)
+	{
+		inicio = novo;
+		fim = novo;
+	}
+	else
+	{
+		fim->prox = novo;
+		fim = novo;
+	}
 
 }
 
 void remove()
 {
-
-
+	if (inicio == NULL)
+	{
+		cout << "Lista Vazia \n";
+	}
+	else
+	{
+		NO* aux = inicio;
+		inicio = inicio->prox;
+		cout << "O numero apagado foi: " << aux->valor;
+		free(aux);
+	}
 
 }
 
+void exibirElementos()
+{
+	if (inicio == NULL) 
+	{
+		cout << "Lista vazia \n";
+		return;
+	}
+	else 
+	{
+		cout << "Elementos: \n";
+		NO* aux = inicio;
+		while (aux != NULL) 
+		{
+			cout << aux->valor << endl;
+			aux = aux->prox;
+		}
+	}
+}
+//Complemente a função insere para que ela inclua um elemento na fila(no final).
+//Implemente a função remove que deve exibir e remover o primeiro elemento da fila ou exibir "Fila Vazia" 
+//se não houver nenhum elemento.
